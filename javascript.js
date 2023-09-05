a475c3fd0d3a840a982a60e1d7dad3bfcd67586b
tysonrobles33 <tysonrobles33@gmail.com>
2023-09-05 11:15:30 -0500

    createGrid() Now Makes A 16 x 16 Grid of Even Divs

--- javascript.js
+++ javascript.js
@@ -6,8 +6,16 @@ const container = document.getElementById('container');
 function createGrid () {
     for (let r = 0; r < 16; r++) {
         let row = document.createElement('div');
-        row.style.height = (screenHeight / 16) + "px";
+        row.setAttribute('style', 'display: flex;');
+        row.style.flexDirection = 'row'
+        row.style.height = (screenHeight / 16) + "px"; /* fits all 16 rows evenly across screen height */
         container.appendChild(row);
+
+        for (let i = 0; i < 16; i++) {                 /* adds colums or items within each row */
+            let item = document.createElement('div');
+            item.style.flex = "1 1 auto"
+            row.appendChild(item)
+        }
     }
 }
 
