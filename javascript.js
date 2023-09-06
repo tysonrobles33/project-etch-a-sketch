4aa77751ddac7279e56c50f8dca61cb16d13730e
tysonrobles33 <tysonrobles33@gmail.com>
2023-09-06 10:30:30 -0500

    Added changeColor() Function That Only Paints Divs When The Mouse Is Held Down

--- javascript.js
+++ javascript.js
@@ -2,7 +2,8 @@ let screenHeight = screen.height;
 let screenwidth = screen.width;
 
 const container = document.getElementById('container');
-
+let gridItem = document.querySelectorAll('.gridItem')
+let item = document.createElement('div');
 function createGrid () {
     for (let r = 0; r < 16; r++) {
         let row = document.createElement('div');
@@ -11,20 +12,34 @@ function createGrid () {
         row.style.height = (screenHeight / 16) + "px"; /* fits all 16 rows evenly across screen height */
         container.appendChild(row);
 
-        for (let i = 0; i < 16; i++) {                 /* adds colums or items within each row */
-            let item = document.createElement('div');
-            item.style.flex = "1 1 auto";
-            row.appendChild(item);
-            item.addEventListener('onmouseover', hover());
+    for (let i = 0; i < 16; i++) {                 /* adds colums or items within each row */
+        let item = document.createElement('div');
+        item.style.flex = "1 1 auto";
+        item.classList.add('gridItem');
+        row.appendChild(item);
         }
     }
 }
 
-function hover() {
-    console.log('moused')
-}
+function changeColor() {
+    let gridItems = document.querySelectorAll('.gridItem')
+    let changeColor = false
 
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener('mousedown', () => {
+            gridItem.style.backgroundColor = 'black';
+            changeColor = true;
+        });
+        gridItem.addEventListener('mouseup', () => {
+            changeColor = false;
+        });
+        gridItem.addEventListener('mouseover', () => {
+            if (changeColor == true) {
+                gridItem.style.backgroundColor = 'black';
+            }
+        })
+    })
+}  
 
-
-hover()
 createGrid()
+changeColor()
